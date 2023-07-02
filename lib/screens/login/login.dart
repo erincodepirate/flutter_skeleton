f1ce3aabc25407cdca254b91585ae7dd079cf62e
Erin Clark <erincodepirate@gmail.com>
2023-07-02 16:20:21 -0700

    Make login screen stateful

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -4,9 +4,14 @@ import 'login_desktop.dart';
 import 'login_mobile.dart';
 import 'login_tablet.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
